c25d5a86d8e0d97df1fb940992492ec555a7cc91
Tim Brand <tim.brand@code.berlin>
2026-09-11 18:28:27 +0200

    Datenbankbeziehungen für Artikel und Kommentare ergänzt

---
+++ database/migrations/2026_09_11_162438_add_user_id_and_is_published_to_articles_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            $table->foreignId('user_id')
+                ->constrained('users')
+                ->onDelete('cascade');
+
+            $table->boolean('is_published')->default(false);
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+            $table->dropColumn(['user_id', 'is_published']);
+        });
+    }
+};
