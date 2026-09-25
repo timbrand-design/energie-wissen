c7036b506ff7565d662cf9ed2e8208cd3a633cf0
Tim Brand <tim.brand@code.berlin>
2026-09-25 19:29:50 +0200

    Artikeltitel in Edit-View anzeigen

---
+++ resources/views/articles/edit.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <title>Artikel bearbeiten</title>
+</head>
+<body>
+    <h1>Artikel bearbeiten</h1>
+
+    <p>{{ $article->title }}</p>
+</body>
+</html>
